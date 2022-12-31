c2b9b3f294717b5c698d61122ef6a8f5dd533497
Igor Wendel Varela de Almeida <igorwwendel@gmail.com>
2022-12-31 20:11:08 -0300

    .

--- python testes/teste01.py
+++
@@ -1,6 +0,0 @@
-primeironumero = input ('primeiro numero: ')
-segundonumero = input ('segundo numeoro: ')
-s = primeironumero + segundonumero
-print (f'a soma de {primeironumero} + {segundonumero} é igual a: {s}')
-
-
